888fe68b714a65c8510c633c262dec1272e6b873
md-ahmodullah <contactme.ahmod@gmail.com>
2025-01-15 01:52:55 +0600

    Added Poppins Font

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,6 +3,6 @@ import { createBrowserRouter } from "react-router-dom";
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <div className="text-3xl font-bold text-[#E63946]">Hello</div>,
   },
 ]);
