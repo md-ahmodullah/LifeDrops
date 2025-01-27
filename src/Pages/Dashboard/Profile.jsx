ff3f173223f059a22e807a6fc3f6409caadd7c25
md-ahmodullah <contactme.ahmod@gmail.com>
2025-01-27 06:34:56 +0600

    Working On Profile

--- src/Pages/Dashboard/Profile.jsx
+++ src/Pages/Dashboard/Profile.jsx
@@ -1,7 +1,11 @@
 export default function Profile() {
   return (
     <>
-      <h1>Profile</h1>
+      <section className="bg-base-300 flex justify-center items-center min-h-[500px]">
+        <div className="rounded-md border-2 border-red-400 p-2 w-1/2 mx-auto">
+          <div className="bg-gradient-to-t from-red-900 to-red-700 rounded-md min-h-80"></div>
+        </div>
+      </section>
     </>
   );
 }
